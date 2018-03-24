d643e72f97d48316e83124ef158fd7cda0351677
Jordan George <jordanngeorge@gmail.com>
2018-03-24 09:28:04 -0400

    make minor edits

--- ext/src/browser_action/browser_action.js
+++ ext/src/browser_action/browser_action.js
@@ -1,10 +1,4 @@
-window.onload = function() {
-
-
-}
-
 var l = document.getElementById("submit");
-
 
 l.onclick = function click() {
 
@@ -22,33 +16,31 @@ l.onclick = function click() {
 			window.open(link)
   });
 
-	};
-/*var t =
-						chrome.tabs.query({active: true}, function(t) {
-						alert(t);
-					});*/
-	var app = angular.module("myApp", []);
-	app.controller("myCtrller", ['$scope', function($scope) {
-	    $scope.captions = [
-				{time: "00:00:04", text: "random captions #1"},
-				{time: "00:00:15", text: "random captions #2"},
-				{time: "00:00:29", text: "random captions #3"}
-			]
+};
 
-				$scope.goToTime = function(index) {
-					var unconvertedTime = $scope.captions[index].time;
-					var a = unconvertedTime.split(':');
-					var timeInSeconds = (+a[0]) * 60 * 60 + (+a[1]) * 60 + (+a[2]);
 
-					// doesn't work with video within playlists
-					var url = window.location.href; // t.url
-					var re = /[^=]*$/; // get everything after the equal sign
-					var videoId = re.exec(url);
-					var link = "https://www.youtube.com/watch?v=" + videoId + "&t=" + timeInSeconds + "s";
+var app = angular.module("myApp", []);
+app.controller("myCtrller", ['$scope', function($scope) {
+    $scope.captions = [
+			{time: "00:00:04", text: "random captions #1"},
+			{time: "00:00:15", text: "random captions #2"},
+			{time: "00:00:29", text: "random captions #3"}
+		]
 
-					window.open(link);
-			};
-	}]);
+			$scope.goToTime = function(index) {
+				var unconvertedTime = $scope.captions[index].time;
+				var a = unconvertedTime.split(':');
+				var timeInSeconds = (+a[0]) * 60 * 60 + (+a[1]) * 60 + (+a[2]);
+
+				// doesn't work with video within playlists
+				var url = window.location.href; // t.url
+				var re = /[^=]*$/; // get everything after the equal sign
+				var videoId = re.exec(url);
+				var link = "https://www.youtube.com/watch?v=" + videoId + "&t=" + timeInSeconds + "s";
+
+				window.open(link);
+		};
+}]);
 
 	//yt = document.getElementById("movie_player")
 	// yt.seekTo(time) **time in seconds
