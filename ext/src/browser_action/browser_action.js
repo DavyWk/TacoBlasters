e75929ceb4e5e5601f670f7df59d90a87005668c
Davy.W <cnetadev@hotmail.fr>
2018-03-24 06:41:45 -0400

    import angular.js

--- ext/src/browser_action/browser_action.js
+++ ext/src/browser_action/browser_action.js
@@ -12,5 +12,30 @@ l.onclick = function goToTime() {
 		window.open(link);
 	};
 	
+	var app = angular.module("myApp", []);
+	app.controller("myCtrller", ['$scope', function($scope) {
+	    $scope.captions = [
+				{time: "00:00:04", text: "random captions #1"},
+				{time: "00:00:15", text: "random captions #2"},
+				{time: "00:00:29", text: "random captions #3"}
+			]
+
+			$scope.goToTime = function(index) {
+					var unconvertedTime = $scope.captions[index].time;
+					var a = unconvertedTime.split(':');
+					var timeInSeconds = (+a[0]) * 60 * 60 + (+a[1]) * 60 + (+a[2]);
+
+					// doesn't work with video within playlist
+					var url = window.location.href;
+					var re = /[^=]*$/; // get everything after the equal sign
+					var videoId = re.exec(url);
+					// console.log(videoId);
+					// var videoId = "Mp9u9fPiVHk";
+					var link = "https://www.youtube.com/watch?v=" + videoId + "&t=" + timeInSeconds + "s";
+
+					window.open(link);
+			};
+	}]);
+	
 	//yt = document.getElementById("movie_player")
 	// yt.seekTo(time) **time in seconds
